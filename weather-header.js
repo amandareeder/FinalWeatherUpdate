43c25ee0827b1cd106452ef5d5b299072138f463
amandareeder <reederamanda08@gmail.com>
2026-02-02 09:19:18 -0700

    Final weather update
    Created a new repository as opposed to editing existing one for ease of finding both

---
+++ weather-header.js
@@ -0,0 +1,31 @@
+const apiKey = "7b0d58637c1c36b220915d55f06438df";
+
+const weatherURL =
+  `https://api.openweathermap.org/data/2.5/weather?q=Tooele&units=imperial&appid=${apiKey}`;
+
+fetch(weatherURL)
+  .then(response => response.json())
+  .then(data => {
+    document.getElementById("five-city").textContent = data.name;
+    document.getElementById("current-desc").textContent = data.weather[0].description;
+    document.getElementById("current-temp").textContent = Math.round(data.main.temp);
+    document.getElementById("current-feelsLike").textContent = Math.round(data.main.feels_like);
+    document.getElementById("current-humid").textContent = data.main.humidity;
+    document.getElementById("current-windSpeed").textContent = Math.round(data.wind.speed);
+
+    const temp = data.main.temp;
+    const speed = data.wind.speed;
+
+    let windChill = "N/A";
+    if (temp <= 50 && speed > 3) {
+      windChill = Math.round(
+        35.74 +
+        0.6215 * temp -
+        35.75 * Math.pow(speed, 0.16) +
+        0.4275 * temp * Math.pow(speed, 0.16)
+      );
+    }
+
+    document.getElementById("current-windChill").textContent = windChill;
+  })
+  .catch(error => console.error("Weather API Error:", error));
